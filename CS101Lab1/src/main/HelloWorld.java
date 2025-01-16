ac5160412e973320dc4c3077143ef7300d95c1e6
waldososo <Warrensutton98@gmail.com>
2025-01-16 15:09:07 -0500

    I didn't have java :(
    its fixed now including java

---
+++ CS101Lab1/src/main/HelloWorld.java
@@ -0,0 +1,11 @@
+package main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Do IT! Just Do It. Don't let your dreams be dreams!
+
+		System.out.println("Hello, World!");
+	}
+
+}
